89942f32147f593406573b57999412ab2a0ad5d1
bishwajit01 <bishwajit.vikram@gmail.com>
2020-04-10 01:10:41 +0530

    Reformatted the LocationStat Model class.

--- corona-virus-tracker/src/main/java/com/vikram/bishwajit/coronavirustracker/models/LocationStats.java
+++ corona-virus-tracker/src/main/java/com/vikram/bishwajit/coronavirustracker/models/LocationStats.java
@@ -5,26 +5,11 @@ package com.vikram.bishwajit.coronavirustracker.models;
  *
  */
 public class LocationStats {
-	
-	private String province;
+
 	private String country;
 	private int totalNumberOfConfirmedCasesPerCountry;
 	private int totalNumberOfDeathCasesPerCountry;
 	private int totalNumberOfRecoveredCasesPerCountry;
-
-	/**
-	 * @return the province
-	 */
-	public String getProvince() {
-		return province;
-	}
-
-	/**
-	 * @param province the province to set
-	 */
-	public void setProvince(String province) {
-		this.province = province;
-	}
 
 	/**
 	 * @return the country
@@ -40,50 +25,26 @@ public class LocationStats {
 		this.country = country;
 	}
 
-	/**
-	 * @return the totalNumberOfConfirmedCasesPerCountry
-	 */
 	public int getTotalNumberOfConfirmedCasesPerCountry() {
 		return totalNumberOfConfirmedCasesPerCountry;
 	}
 
-	/**
-	 * @param totalNumberOfConfirmedCasesPerCountry the
-	 *                                              totalNumberOfConfirmedCasesPerCountry
-	 *                                              to set
-	 */
 	public void setTotalNumberOfConfirmedCasesPerCountry(int totalNumberOfConfirmedCasesPerCountry) {
 		this.totalNumberOfConfirmedCasesPerCountry = totalNumberOfConfirmedCasesPerCountry;
 	}
 
-	/**
-	 * @return the totalNumberOfDeathCasesPerCountry
-	 */
 	public int getTotalNumberOfDeathCasesPerCountry() {
 		return totalNumberOfDeathCasesPerCountry;
 	}
 
-	/**
-	 * @param totalNumberOfDeathCasesPerCountry the
-	 *                                          totalNumberOfDeathCasesPerCountry to
-	 *                                          set
-	 */
 	public void setTotalNumberOfDeathCasesPerCountry(int totalNumberOfDeathCasesPerCountry) {
 		this.totalNumberOfDeathCasesPerCountry = totalNumberOfDeathCasesPerCountry;
 	}
 
-	/**
-	 * @return the totalNumberOfRecoveredCasesPerCountry
-	 */
 	public int getTotalNumberOfRecoveredCasesPerCountry() {
 		return totalNumberOfRecoveredCasesPerCountry;
 	}
 
-	/**
-	 * @param totalNumberOfRecoveredCasesPerCountry the
-	 *                                              totalNumberOfRecoveredCasesPerCountry
-	 *                                              to set
-	 */
 	public void setTotalNumberOfRecoveredCasesPerCountry(int totalNumberOfRecoveredCasesPerCountry) {
 		this.totalNumberOfRecoveredCasesPerCountry = totalNumberOfRecoveredCasesPerCountry;
 	}
