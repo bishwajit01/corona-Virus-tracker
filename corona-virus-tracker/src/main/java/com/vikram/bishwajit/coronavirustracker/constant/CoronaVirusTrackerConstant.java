5c217c64850e08a57b9a6f1f8c6de9de14eb7ca2
bishwajit01 <bishwajit.vikram@gmail.com>
2020-04-13 20:36:36 +0530

    Added a new constant file, that has to be read from the local.

--- corona-virus-tracker/src/main/java/com/vikram/bishwajit/coronavirustracker/constant/CoronaVirusTrackerConstant.java
+++ corona-virus-tracker/src/main/java/com/vikram/bishwajit/coronavirustracker/constant/CoronaVirusTrackerConstant.java
@@ -8,4 +8,5 @@ public class CoronaVirusTrackerConstant {
 	public static final String CONFIRMED_CASES_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 	public static final String DEATH_CASES_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
 	public static final String RECOVERED_CASES_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv";
+	public static final String LCOAL_CSV_FILE= "C:\\Users\\bvikram2\\Desktop\\data.csv";
 }
